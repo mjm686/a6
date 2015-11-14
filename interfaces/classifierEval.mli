76661a178af4a33b835a6373d3f13f36b203dd93
Maggie Bi <byunjie@gmail.com>
2015-11-14 15:46:30 -0500

    design doc and interfaces

---
+++ interfaces/classifierEval.mli
@@ -0,0 +1,15 @@
+open CrossValid
+open ExpGradient
+open Parser
+open KNN
+open DecisionTree
+open Bayes
+
+(** [eval] runs 4-fold cross validation with the EG algorithm to give weights
+ *  to all three algorithms based on their performances, and then each 
+ *  algorithm would receive the average weight of the four weights
+ *
+ *  [eval d] takes the parsed traning data and returns a set of weights for
+ *  outputting the final predictions for the testing data
+ * *)
+val eval : data list -> weights
