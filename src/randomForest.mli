3dae2e5d932e352e68b0f9cfe5c7f4140a0be1c9
jrp328 <jrp328@cornell.edu>
2015-11-28 20:34:21 -0500

    Update randomForest.mli

--- src/randomForest.mli
+++ src/randomForest.mli
@@ -2,23 +2,114 @@ open Parser
 open rForest
 
 (**
- * [listCats dat tl] returns a new tree list based on
+ * [getLeafs1 aY tl] returns a new tree list based on
+ * attr aY and tree list tl.
+ *
+ * This tree list will be the list of Leafs
+ * corresponding to the Node containing aY.
+ *)
+val getLeafs1 : attr -> tree list -> tree list
+
+(**
+ * [getLeafs2 aX aY tl] returns a new tree list
+ * based on attr aY and aX and tree list tl.
+ *
+ * This tree list will be the list of Leafs
+ * corresponding to the Node containing aY, whose
+ * parent node contains aX.
+ *)
+val getLeafs2 : attr -> attr -> tree list
+-> tree list
+
+(**
+ * [getLeafs3 aTime aX aY tl] returns a new
+ * tree list based on attr aTime, aY, and aX and
+ * tree list tl.
+ *
+ * This tree list will be the list of Leafs
+ * corresponding to the Node containing aY, whose
+ * parent node contains aX, whose parent node
+ * contains aTime.
+ *)
+val getLeafs3 : attr -> attr -> attr -> tree list
+-> tree list
+
+(**
+ * [getLeafs4 aDate aTime aX aY tl] returns a new
+ * tree list based on attr aDate, aTime, aY,
+ * and aX and tree list tl.
+ *
+ * This tree list will be the list of Leafs
+ * corresponding to the Node containing aY, whose
+ * parent node contains aX, whose parent node
+ * contains aTime, whose parent node contains
+ * aDate.
+ *)
+val getLeafs4 : attr -> attr -> attr -> attr
+-> tree list -> tree list
+
+(**
+ * [getLeafs dat tl] returns a new tree list based on
  * data dat and tree list tl.
  *
- * This tree list will be the list of crime categories
+ * This tree list will be the list of Leafs
  * corresponding to the location and time given in
- * dat (a list of Leafs).
+ * dat.
+ *
+ * [getLeafs] uses the other getLeafs functions
+ * as helpers, and essentially runs [getLeafs4]
+ * using information from dat as arguments.
  *)
-val listCats : data -> tree list -> tree list
+val getLeafs : data -> tree list -> tree list
+
+(**
+ * [getI lf] returns the value lf.i of Leaf i.
+ *)
+val getI : tree -> int
 
 (**
  * [total tl] returns a the total number of crimes
- * recorded for a certain location and time.
+ * recorded for a given list of Leafs tl.
  *
  * Tree list tl should be a list acquired by using
- * the above function listCats.
+ * the above function getLeafs.
  *)
 val total : tree list -> int
+
+(**
+ * [divideIntoFloat i1 i2] converts integers i1
+ * and i2 into floats and divides them.
+ *)
+val divideIntoFloat : int -> int -> float
+
+(**
+ * [singlePair tl lf] returns a pair containing a
+ * category and a float.
+ *
+ * The category is lf.c and the float is
+ * lf.i divided by the total number of crimes in
+ * list tl, assuming lf is a Leaf. If Leaf lf is
+ * in list tl, the float will represent the
+ * percentage of crimes of type lf.c in the
+ * list tl.
+ *
+ * If lf is a Node, the pair returned will instead
+ * contain category UNDETERMINED and float 0.
+ *)
+val singlePair : tree list -> tree -> cat * float
+
+(**
+ * [listPair tl tlr] returns a list of pairs
+ * containing a category and a float.
+ *
+ * listPair takes in a list of trees (both tl
+ * and tlr should be the same tree list) and
+ * outputs a list of pairs; each pair is
+ * the result of [singlePair] being applied
+ * to an element of tl.
+ *)
+val listPair : tree list -> tree list
+-> (cat * float) list
 
 (**
  * [predict dat tl] returns a list of pairs of categories
@@ -33,7 +124,7 @@ val total : tree list -> int
 val predict : data -> tree list -> (cat*float) list
 
 (**
- * [predictions dl tl] returns a list of lists of pairs
+ * [predict dl tl] returns a list of lists of pairs
  * of categories and floats.
  *
  * predictions runs [predict] on each element of dl
